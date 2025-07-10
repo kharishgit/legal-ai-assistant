cc2635d64dabb5dbb4a07d38acf63e8b86026a15
harish.k3 <harish.k@pixdynamics.com>
2025-07-10 08:53:26 +0530

    Created new rag_chain.py and renamed the old one to old_rag_chain.py

--- tests/test_chromadb.py
+++ tests/test_chromadb.py
@@ -1,11 +1,16 @@
-from langchain_chroma import Chroma
-from langchain_huggingface import HuggingFaceEmbeddings
+# from langchain_chroma import Chroma
+# from langchain_huggingface import HuggingFaceEmbeddings
 
-vectorstore = Chroma(
-    persist_directory="data/vector_db",
-    embedding_function=HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2",
-        model_kwargs={"device": "cpu"}
-    )
-)
-print(len(vectorstore.get()["ids"]))
+# vectorstore = Chroma(
+#     persist_directory="data/vector_db",
+#     embedding_function=HuggingFaceEmbeddings(
+#         model_name="sentence-transformers/all-MiniLM-L6-v2",
+#         model_kwargs={"device": "cpu"}
+#     )
+# )
+# print(len(vectorstore.get()["ids"]))
+
+from langchain_chroma import Chroma
+vectorstore = Chroma(persist_directory="data/vector_db")
+docs = vectorstore.get()['documents']
+print(docs[0])  # Check if page_content is a string
